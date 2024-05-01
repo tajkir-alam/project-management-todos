c47c39f3221e75669785a3298c155aa1a86f5be4
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2024-05-01 22:04:50 +0600

    add task modal designed

--- src/app/Component/AuthorizedComponent/Tasks/Task.js
+++ src/app/Component/AuthorizedComponent/Tasks/Task.js
@@ -1,14 +1,18 @@
 import taskStore from "@/app/zustand/store";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   FieldTimeOutlined,
   EyeOutlined,
   EditOutlined,
 } from "@ant-design/icons";
 import Image from "next/image";
-import { Tooltip } from "antd";
+import { Tooltip, Button, Form, Modal, Input, DatePicker, Select } from "antd";
 
 const Task = ({ projectID, status, searchQuery }) => {
+  const { Option } = Select;
+  const [form] = Form.useForm();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedWorkers, setSelectedWorkers] = useState([]);
   const fetchTask = taskStore((state) => state.fetchTask);
   const tasks = taskStore((state) => state.tasks);
   const setDraggedTask = taskStore((store) => store.setDraggedTask);
@@ -25,6 +29,14 @@ const Task = ({ projectID, status, searchQuery }) => {
   useEffect(() => {
     fetchTask();
   }, [fetchTask]);
+
+  const handleWorkerChange = (selectedOptions) => {
+    setSelectedWorkers(selectedOptions);
+  };
+
+  const onFinish = (values) => {
+    console.log(values);
+  };
 
   return filterTasks.map((task, index) => (
     <div
@@ -67,9 +79,68 @@ const Task = ({ projectID, status, searchQuery }) => {
               <EyeOutlined className="text-blue-800 text-lg" />
             </Tooltip>
           </button>
-          <button className="btn">
+          <button onClick={() => setIsModalOpen(!isModalOpen)} className="btn">
             <EditOutlined className="text-green-500 text-lg" />
           </button>
+          <Modal
+            title="Add Task"
+            open={isModalOpen}
+            footer={[
+              <Button
+                key="addTask"
+                type="primary"
+                onClick={() => form.submit()}
+              >
+                Add Task
+              </Button>,
+              <Button key="cancel" onClick={() => setIsModalOpen(false)}>
+                Cancel
+              </Button>,
+            ]}
+          >
+            <Form form={form} layout="vertical" onFinish={onFinish}>
+              <Form.Item
+                name="taskTitle"
+                label="Task Title"
+                rules={[{ required: true, message: "Please enter task title" }]}
+              >
+                <Input defaultValue={task.taskTitle} />
+              </Form.Item>
+              <Form.Item
+                name="taskDescription"
+                label="Task Description"
+                rules={[
+                  { required: true, message: "Please enter task description" },
+                ]}
+              >
+                <Input.TextArea defaultValue={task.taskDescription} />
+              </Form.Item>
+              <Form.Item
+                name="taskWorker"
+                label="Task Worker"
+                rules={[
+                  { required: true, message: "Please select task workers" },
+                ]}
+              >
+                <Select
+                  mode="multiple"
+                  placeholder="Select task workers"
+                  onChange={handleWorkerChange}
+                >
+                  <Option value="user1">User 1</Option>
+                  <Option value="user2">User 2</Option>
+                  <Option value="user3">User 3</Option>
+                </Select>
+              </Form.Item>
+              <Form.Item
+                name="deadlines"
+                label="Deadline"
+                rules={[{ required: true, message: "Please select deadline" }]}
+              >
+                <DatePicker format="DD/MM/YY" />
+              </Form.Item>
+            </Form>
+          </Modal>
         </div>
       </div>
     </div>
